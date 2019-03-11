c2bdf7855a862f15b36502debe7430d2a9c5fbfa
袁熙彤 <17722038@bjtu.edu.cn>
2019-03-11 23:20:35 +0800

    2.9

--- supi/admin.py
+++ supi/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-from .models import zhuanye,student
+from .models import Major,Student
 
 
-class zhuanyeAdmin(admin.ModelAdmin):
-    list_display=['pk','zname','zgirlnum','zboynum','isDelete','school']
-    list_filter=['zname']
-    search_fields=['zname']
+class MajorAdmin(admin.ModelAdmin):
+    list_display=['pk','major','num_of_women','num_of_men','isDelete','school']
+    list_filter=['major']
+    search_fields=['major']
     list_per_page=7
 
 
     fieldsets=[
-        ("base",{"fields":['zname','isDelete',]}),
-        ("num",{"fields":['zgirlnum','zboynum']})
+        ("base",{"fields":['major','isDelete',]}),
+        ("num",{"fields":['num_of_women','num_of_men']})
     ]
-admin.site.register(zhuanye,zhuanyeAdmin)
+admin.site.register(Major,MajorAdmin)
 
-class studentsAdmin(admin.ModelAdmin):
+class StudentsAdmin(admin.ModelAdmin):
 
     def gender(self):
-        if self.sgender:
+        if self.gender:
             return"男"
         else:
             return"女"
-    list_display=['sid','sname','zhuanye',gender,'school','isDelete']
-    list_filter=['sname']
-    search_fields=['sname']
+    list_display=['sid','student_name','major',gender,'school','isDelete']
+    list_filter=['student_name']
+    search_fields=['student_name']
     list_per_page=6
-admin.site.register(student,studentsAdmin)
+admin.site.register(Student,StudentsAdmin)
 
--- supi/models.py
+++ supi/models.py
@@ -1,27 +1,27 @@
 from django.db import models
 
 # Createyourmodelshere.
-class zhuanye(models.Model):
-    zname = models.CharField(verbose_name="专业",max_length=20)
-    zgirlnum = models.IntegerField(verbose_name="男生数",db_column="男生数")
-    zboynum = models.IntegerField(verbose_name="女生数",db_column="女生数")
+class Major(models.Model):
+    major = models.CharField(verbose_name="专业",max_length=20)
+    num_of_men = models.IntegerField(verbose_name="男生数",db_column="男生数")
+    num_of_women = models.IntegerField(verbose_name="女生数",db_column="女生数")
     school  = models.CharField(max_length=15, null=True,verbose_name="学校",db_column="学校")
     isDelete = models.BooleanField(default=False,verbose_name="是否删除",db_column="是否删除")
     def __str__(self):
-        return self.zname
+        return self.major
     class Meta:
         db_table="majors"
         ordering = ['id']
-class student(models.Model):
-    sname = models.CharField(max_length=20,db_index=True,verbose_name="姓名",db_column="姓名")
+class Student(models.Model):
+    student_name = models.CharField(max_length=20,db_index=True,verbose_name="姓名",db_column="姓名")
     sid = models.IntegerField(null=False,primary_key=True,default="0",verbose_name="学号",db_column="学号")
-    zhuanye = models.ForeignKey('zhuanye',on_delete=models.CASCADE,null=True,verbose_name="专业",db_column="专业")
+    major= models.ForeignKey('major',on_delete=models.CASCADE,null=True,verbose_name="专业",db_column="专业")
     school = models.CharField(max_length=15, null=True,verbose_name="学校",db_column="学校")
-    sgender = models.BooleanField(default=False,verbose_name="性别",db_column="性别")
+    gender = models.BooleanField(default=False,verbose_name="性别",db_column="性别")
     isDelete = models.BooleanField(default=False,verbose_name="是否删除",db_column="是否删除")
 
     def __str__(self):
-        return self.sname
+        return self.student_name
     class Meta:
         db_table="students"
         ordering=['sid']
